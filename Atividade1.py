531682d683e77c0b8d13aa1707dd21c9a2a69776
FK1FF3R <flavianokt@gmail.com>
2025-05-09 22:11:36 -0300

    :sparkles: First Commit

---
+++ Atividade1.py
@@ -0,0 +1,26 @@
+# 1- Programa de Saudação
+print("Olá, mundo!")
+
+# 2- Calculadora de Soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é: {soma}")
+
+# 3- Calculadora de Volume
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa retangular é: {volume} cm³")
+
+# 4- Calculadora de Preço Total
+nome_produto = "Caderno"
+preco_unitario = 12.40
+quantidade = 6
+preco_total = preco_unitario * quantidade
+
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
