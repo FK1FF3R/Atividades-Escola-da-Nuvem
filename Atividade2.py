05181607592abc033a67052c5edf22a5436b8f02
FK1FF3R <flavianokt@gmail.com>
2025-05-25 22:41:48 -0300

    :sparkles: feat: Adicao atividade 2

---
+++ Atividade2.py
@@ -0,0 +1,10 @@
+# Define as variáveis com os números a serem somados
+numero1 = 12
+numero2 = 14
+
+# Calcula a soma dos dois números
+resultado = numero1 + numero2
+
+print(f"O primeiro número é: {numero1}")
+print(f"O segundo número é: {numero2}")
+print(f"A soma de {numero1} + {numero2} é: {resultado}")
